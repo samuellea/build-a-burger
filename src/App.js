cb00e3a8cd1d3a62866f8e89792746f624f42b2f
samuellea <samuel.lea@live.co.uk>
2020-10-13 18:21:51 +0100

    Added route guarding, so unauthenticated users cannot access Orders page

--- src/App.js
+++ src/App.js
@@ -11,40 +11,29 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 class App extends Component {
   render() {
-    // let routes = (
-    //   <Switch>
-    //     <Route exact path="/">
-    //       <Redirect to="/burgerbuilder" />
-    //     </Route>
-    //     <Route path="/burgerbuilder" component={BurgerBuilder} />
-    //     <Route path="/auth" component={Auth} />
-    //     <Redirect to="/" />
-    //     <Route path="/checkout" component={Checkout} />
-    //     <Route path="/orders" component={Orders} />
-    //     <Route path="/logout" component={Logout} />
-    //     {/* <Redirect to="/" /> */}
-    //   </Switch>
-    // );
-    // if (this.props.isAuthenticated) {
-    //   routes = (
-
-    //   );
-    // }
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route exact path="/">
+          <Redirect to="/burgerbuilder" />
+        </Route>
+      </Switch>
+    );
+    if (this.props.isAuthenticated) (
+      <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
+        <Route exact path="/">
+          <Redirect to="/burgerbuilder" />
+        </Route>
+      </Switch>
+    );
     return (
       <BrowserRouter>
         <div>
           <Layout>
-            <Switch>
-              <Route exact path="/">
-                <Redirect to="/burgerbuilder" />
-              </Route>
-              <Route path="/burgerbuilder" component={BurgerBuilder} />
-              <Route path="/auth" component={Auth} />
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
-              <Route path="/logout" component={Logout} />
-              {/* <Redirect to="/" /> */}
-            </Switch>
+            {routes}
           </Layout>
         </div>
       </BrowserRouter>
